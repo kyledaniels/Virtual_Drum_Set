ed74ac47dfb13d3891da381052a694206826da51
kyle67 <daniels.kyle@gmail.com>
2020-02-17 17:22:17 -0500

    Modified sound and images on drum kit

--- index.js
+++ index.js
@@ -29,37 +29,37 @@ function makeSound(key){
 
     switch (key) {
         case "w":
-         var tom1 = new Audio("../Drum Kit Starting Files/sounds/tom-1.mp3");
+         var tom1 = new Audio("../Virtual_Drum_Set/sounds/tom-1.mp3");
          tom1.play();
             break;
             
         case "a":
-         var tom2 = new Audio("../Drum Kit Starting Files/sounds/tom-2.mp3");
+         var tom2 = new Audio("../Virtual_Drum_Set/sounds/tom-2.mp3");
          tom2.play();
             break;
 
         case "s":
-        var tom3 = new Audio("../Drum Kit Starting Files/sounds/tom-3.mp3");
+        var tom3 = new Audio("../Virtual_Drum_Set/sounds/tom-3.mp3");
         tom3.play();
                 break;
 
         case "d":
-        var tom4 = new Audio("../Drum Kit Starting Files/sounds/tom-4.mp3");
+        var tom4 = new Audio("../Virtual_Drum_Set/sounds/tom-4.mp3");
         tom4.play();        
             break;
 
         case "j":
-        var snare = new Audio("../Drum Kit Starting Files/sounds/snare.mp3");
+        var snare = new Audio("../Virtual_Drum_Set/sounds/snare.mp3");
         snare.play();
             break;
 
         case "k":
-        var crash = new Audio("../Drum Kit Starting Files/sounds/crash.mp3");
+        var crash = new Audio("../Virtual_Drum_Set/sounds/crash.mp3");
         crash.play();
             break;
 
         case "l":
-        var kick = new Audio("../Drum Kit Starting Files/sounds/kick-bass.mp3");
+        var kick = new Audio("../Virtual_Drum_Set/sounds/kick-bass.mp3");
         kick.play();
             break;
   
