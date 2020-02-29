7f8e1ad7e225b52ff75f613c90de5f6f71929b74
kyle67 <daniels.kyle@gmail.com>
2020-02-29 15:49:45 -0500

    Added animation to drum set

--- index.js
+++ index.js
@@ -1,85 +1,81 @@
-// Detecting Buttons Pressed
-
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for(i=0;i<numberOfDrumButtons;i++) {
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
-document.querySelectorAll(".drum")[i].addEventListener("click", function(){
- 
-  var buttonInnerHTML = this.innerHTML;
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-  makeSound (buttonInnerHTML);
-  buttonAnimation(buttonInnerHTML);
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
+
+}
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
 
 });
-    
-  
 
-}
 
-// Detecting KeyBoard Pressed
+function makeSound(key) {
 
-document.addEventListener("keydown", function (event){
-   makeSound(event.key);
-   buttonAnimation(event.key);
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
 
-});
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
 
-function makeSound(key){
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
 
-    switch (key) {
-        case "w":
-         var tom1 = new Audio("../Virtual_Drum_Set/sounds/tom-1.mp3");
-         tom1.play();
-            break;
-            
-        case "a":
-         var tom2 = new Audio("../Virtual_Drum_Set/sounds/tom-2.mp3");
-         tom2.play();
-            break;
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
 
-        case "s":
-        var tom3 = new Audio("../Virtual_Drum_Set/sounds/tom-3.mp3");
-        tom3.play();
-                break;
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
 
-        case "d":
-        var tom4 = new Audio("../Virtual_Drum_Set/sounds/tom-4.mp3");
-        tom4.play();        
-            break;
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
 
-        case "j":
-        var snare = new Audio("../Virtual_Drum_Set/sounds/snare.mp3");
-        snare.play();
-            break;
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
 
-        case "k":
-        var crash = new Audio("../Virtual_Drum_Set/sounds/crash.mp3");
-        crash.play();
-            break;
 
-        case "l":
-        var kick = new Audio("../Virtual_Drum_Set/sounds/kick-bass.mp3");
-        kick.play();
-            break;
-  
+    default: console.log(key);
 
-        default: console.log(buttonInnerHTML);
-    }  
-
-}
-
-function buttonAnimation(currentKey){
-
-    var activeButton = document.querySelector("." + currentKey);
-
-      activeButton.classList.add("pressed");
-      setTimeout(function(){
-          activeButton.classList.remove("pressed");
-      },100);
-      
+  }
 }
 
 
+function buttonAnimation(currentKey) {
 
-     
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
